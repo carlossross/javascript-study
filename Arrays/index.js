a64b2378845ea17947aa535a02a2ae63b6976b2c
carlossross <carlos.rossv@gmail.com>
2021-07-03 22:41:26 -0500

    Adding Arrays  concepts

---
+++ Arrays/index.js
@@ -0,0 +1,44 @@
+// Array Literal
+
+const pokemonTeam = [
+  "Charizard",
+  "Mimikyu",
+  "Dragapult",
+  "Torterra",
+  "Pikachu",
+  "Mewtwo",
+];
+
+// Copy array with 'spread' operator
+
+const pokemonCopy = [...pokemonTeam];
+
+//console.log(pokemonCopy);
+
+// String to array
+const pokemonStrings = [..."pikachu"];
+
+//console.log(pokemonStrings);
+
+// Remove duplicates
+
+const duplicates = [..."abcdeefgahijkbope"];
+const noDuplicates = [...new Set(duplicates)];
+
+// console.log(noDuplicates);
+
+// Array constructor
+//Empty Array
+const empty = new Array();
+
+// Preallocate
+const a = new Array(10);
+//console.log(a);
+
+const preallocate = new Array(1, 2, 3, 4, 5, 6, "testing, testing");
+
+//console.log(preallocate);
+
+// Array.of()
+const single = Array.of(3);
+console.log(single);

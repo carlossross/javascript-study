3afc3a86007ea090cd7898be921c8e2fb991bb69
Carlos <39545296+carlossross@users.noreply.github.com>
2021-07-05 00:11:57 -0500

    Adding iterator methods

--- Arrays/index.js
+++ Arrays/index.js
@@ -42,3 +42,50 @@ const preallocate = new Array(1, 2, 3, 4, 5, 6, 'testing, testing');
 // Array.of()
 const single = Array.of(3);
 //console.log(single);
+
+//Array Methods
+
+//forEach()
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+let sum = 0;
+
+myArray.forEach((value) => {
+  sum += value;
+});
+
+//console.log(sum);
+
+myArray.forEach((value, index, myArray) => {
+  myArray[index] = value + 1;
+});
+
+//console.log(myArray);
+
+//map()
+
+const mapArray = [1, 2, 5];
+const cuadradoArray = mapArray.map((m) => m * m);
+//console.log(cuadradoArray);
+const mapped = mapArray.map((m) => {
+  return { [m]: m }; // computed property
+});
+//console.log(mapped);
+
+//filter()
+const numbers = [2, 5, 28, 68, 45, 25, 235, 9, 132, 5, 2];
+const oddNumbers = numbers.filter((number) => number % 2 > 0);
+//console.log(oddNumbers);
+
+//find() and findIndex()
+const numberArray = [1, 2, 3];
+const index = numberArray.findIndex((x) => x > 2);
+//console.log(index);
+const founded = numberArray.find((x) => x > 2);
+//console.log(founded);
+
+//every() and some()
+const aarray = [1, 2, 3, 4, 5];
+const allLessThan10 = aarray.every((x) => x < 10);
+//console.log(allLessThan10);
+const someMoreThan4 = aarray.some((x) => x > 4);
+//console.log(someMoreThan4);
